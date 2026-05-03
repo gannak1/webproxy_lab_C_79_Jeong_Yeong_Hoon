20fe3b1b766ea3717f9db1f21b1c6f5de044ac7b
gannak1 <gi676788@gmail.com>
2026-05-03 23:48:56 +0900

    2

--- ccc.c
+++ ccc.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
 #include "testa.c"
 #include "testb.c"
+#include <stdio.h>
+#include <string.h>
 
 int main() {
-  printB();
+    char *asd;
+    asd = "asd\\asdas\\dasd";
+    while (strchr(asd, '\\')!='\0') {
+        printf("%d\n", asd);
+        asd++;
+    }
 }
--- testb.c
+++ testb.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
-void printA() {
+void printB() {
   printf("C");
 }
