2d31f8ac7ab18d7176e82b475b158a94ed03ca24
gannak1 <gi676788@gmail.com>
2026-05-04 10:13:06 +0900

    2

--- ccc.c
+++ ccc.c
@@ -4,10 +4,48 @@
 #include <string.h>
 
 int main() {
-    char *asd;
-    asd = "asd\\asdas\\dasd";
-    while (strchr(asd, '\\')!='\0') {
-        printf("%d\n", asd);
-        asd++;
-    }
+    // char *asd;
+    // asd = "hello world";
+    // while (strchr(' ',asd)!=NULL) {
+    //     printf("%s\n", asd);
+    //     asd++;
+    // }
+    // printf("%s\n", asd);
+    long x = 0x47480000-0x4747ffc0;
+    printf("%p",x);
+}
+
+
+
+char *
+strtok_r (char *s, const char *delimiters, char **save_ptr) {
+	char *token;
+
+
+	/* If S is nonnull, start from it.
+	   If S is null, start from saved position. */
+	if (s == NULL)
+		s = *save_ptr;
+
+	/* Skip any DELIMITERS at our current position. */
+	while (strchr (delimiters, *s) != NULL) {
+		/* strchr() will always return nonnull if we're searching
+		   for a null byte, because every string contains a null
+		   byte (at the end). */
+		if (*s == '\0') {
+			*save_ptr = s;
+			return NULL;
+		}
+    s++;
+	}
+	/* Skip any non-DELIMITERS up to the end of the string. */
+	token = s;
+	while (strchr (delimiters, *s) == NULL)
+		s++;
+	if (*s != '\0') {
+		*s = 'P';
+		*save_ptr = s + 1;
+	} else
+		*save_ptr = s;
+	return token;
 }
