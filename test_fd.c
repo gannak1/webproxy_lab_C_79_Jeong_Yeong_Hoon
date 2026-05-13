fc0c9b356f159fc97e88def781591f63e5169b6b
gannak1 <gi676788@gmail.com>
2026-05-14 02:46:18 +0900

    feat: 2

--- test_fd.c
+++ test_fd.c
@@ -1,3 +1,4 @@
+#include "hash.h"
 #include "list.c"
 #include "list.h"
 #include "testa.c"
@@ -6,49 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FNV_64_PRIME 0x00000100000001B3UL
-#define FNV_64_BASIS 0xcbf29ce484222325UL
-
-uint64_t hash_bytes(const void *buf_);
-
 int main() {
-    // for (int s = 0, d = 0; s < 10; s++, d++) {
-    void *addr;
-    addr = (void *)15;
-    // printf("addr : %p\n", addr);
-    // size_t data = (size_t)addr;
-    // unsigned long addr_array[16];
-    // int index = 0;
-    // for (int idx = 15; idx >= 0; idx--) {
-    //     addr_array[idx] = (long)data & 0xf;
-    //     data = data >> 4;
-    // }
-    // printf("addr : ");
-    // for (int idx = 0; idx < 16; idx++) {
-    //     printf("%d ", addr_array[idx]);
-    // }
-    // printf("\n");
-    for (int idx = 0; idx < 10; idx++) {
-        printf("\n\n%d : ", idx);
-        printf("%p\n", addr);
-        printf("out : %p\n\n", hash_bytes(addr + idx * 10));
-    }
+    struct hash spt =
+        hash_init(struct hash * h, hash_hash_func * hash, hash_less_func * less, void *aux);
 }
-// void *va;
-/* Returns a hash of the SIZE bytes in BUF. */
-uint64_t hash_bytes(const void *buf_) {
-    int size = 1;
-    /* Fowler-Noll-Vo 32-bit hash, for bytes. */
-    printf("buf addr : %p\n", (size_t)buf_);
-    size_t data = (size_t)buf_;
-    printf("buf addr : %p\n", data);
-    uint64_t hash;
-    // ASSERT(buf != NULL);
 
-    hash = FNV_64_BASIS;
-    while (size-- > 0) {
-        hash = (hash * FNV_64_PRIME) ^ (data);
-    }
+bool page_less(const struct hash_elem *a_, const struct hash_elem *b_, void *aux UNUSED) {
+    const struct page *a = hash_entry(a_, struct page, hash_elem);
+    const struct page *b = hash_entry(b_, struct page, hash_elem);
 
-    return hash;
+    return a->addr < b->addr;
 }
